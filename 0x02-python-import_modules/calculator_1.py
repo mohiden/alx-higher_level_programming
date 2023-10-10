23db3e279b9771e83252824d85a0d94342481aa3
mohiden <mohidenjama@gmail.com>
2023-10-10 07:30:25 -0700

    0x02 1

---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: int
+        b: int
+
+    Returns:
+        the sum of a and b
+
+    """
+    return a + b
+
+
+def sub(a, b):
+    """My subtraction function
+
+    Args:
+        a: int
+        b: int
+
+    Returns:
+        the subtraction of a and b
+
+    """
+    return a - b
+
+
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: int
+        b: int
+
+    Returns:
+        the multiplication of a and b
+
+    """
+    return a * b
+
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: int
+        b: int
+
+    Returns:
+        the division of a and b
+
+    """
+    return a / b
